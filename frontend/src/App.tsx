c63f1cb73b9acbeb7816379eb4dbeb59cf43300d
João Salvador <j8731615@gmail.com>
2021-05-04 15:10:09 -0300

    basic static componets

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,21 @@
+import Navbar from "assets/css/Componets/Navbar";
+import DataTable from "assets/Componets/DataTable/indes";
+import Footer from "assets/Componets/Footer";
+
+
 
 function App() {
   return (
-    <div>
-      <h1 className="text-primary">Hellow world</h1>
-    </div>
+    <>
+      <Navbar />
+      <div className="container">
+        <h1 className="text-primary">Hellow world</h1>
+      </div>
+
+      <DataTable />
+      <Footer />
+      
+    </>
   );
 }
 
